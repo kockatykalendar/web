3b5dcd640c8c677e5fd9cd7e538ff5d17f082e5a
Matej Novota <matej.novota@gmail.com>
2024-04-22 01:32:34 +0200

    master: render organizers as webcomponents

--- kockatykalendar/web/static/js/homepage.js
+++ kockatykalendar/web/static/js/homepage.js
@@ -9,21 +9,23 @@ const load_json = async url => {
 }
 
 const DEFAULT_ORGANIZERS = ['trojsten', 'p-mat', 'riesky', 'sezam', 'strom']
-const DATA_URL_PREFIX = 'https://data.kockatykalendar.sk/'
+const DATA_URL_PREFIX = 'https://data.kockatykalendar.sk'
 
-let ORGANIZERS = load_json(`${DATA_URL_PREFIX}organizers.json`)
-
+let ORGANIZERS = load_json(`${DATA_URL_PREFIX}/organizers.json`)
 
 
 document.addEventListener('DOMContentLoaded', async () => {
 	ORGANIZERS = await ORGANIZERS
-	const orgs_html = DEFAULT_ORGANIZERS.reduce((html, org) => `${html}
-<article class="border rounded-lg p-8 text-center shadow" style="border-color: ${ORGANIZERS[org].color}">
-	<a href="${ORGANIZERS[org].web}" target="_blank" rel="noreferrer">
-		<img src="${DATA_URL_PREFIX}${ORGANIZERS[org].icon}" alt="${ORGANIZERS[org].name}" class="h-24 mx-auto mb-4">
-	</a>
-	<h2 class="mb-2 font-bold text-2xl">${ORGANIZERS[org].name}</h2>
-		<p>${ORGANIZERS[org].info}</p>
-</article>`, '')
-	document.getElementById('organizations').innerHTML = orgs_html
+	DEFAULT_ORGANIZERS.forEach(org => {
+		let element = document.createElement("organizer-element");
+		element.setAttribute("color", ORGANIZERS[org].color);
+		element.setAttribute("link", ORGANIZERS[org].web);
+		element.setAttribute("icon", `${DATA_URL_PREFIX}/${ORGANIZERS[org].icon}`);
+		element.setAttribute("name", ORGANIZERS[org].name);
+		let info =document.createElement("p");
+		info.textContent = ORGANIZERS[org].info;
+		info.setAttribute("slot", "info");
+		element.appendChild(info);
+		document.getElementById('organizations').appendChild(element);
+	});
 })
---
+++ kockatykalendar/web/static/js/organizer_element.js
@@ -0,0 +1,52 @@
+
+class Organizer extends HTMLElement {
+    static observedAttributes = ["color", "link", "icon", "name"];
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+<style>
+    article {
+        border: 1px solid var(--color, #000);
+        border-radius: 0.5rem;
+        padding: 2rem;
+        text-align: center;
+        height: 100%;
+        box-sizing: border-box;
+    }
+    img {
+        height: 6rem;
+        margin: 0 auto 1rem;
+    }
+    h2 {
+        margin-bottom: 0.5rem;
+        font-weight: 700;
+        font-size: 1.5rem;
+        line-height: 2rem;
+    }
+</style>
+<article>
+	<a href="" target="_blank" rel="noreferrer">
+		<img src="" alt="">
+	</a>
+	<h2 class="mb-2 font-bold text-2xl"></h2>
+	<slot name="info"></slot>
+</article>`;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "color") {
+            this.shadowRoot.querySelector("article").style.setProperty("--color", newValue);
+        } else if (name === "link") {
+            this.shadowRoot.querySelector("a").href = newValue;
+        } else if (name === "icon") {
+            this.shadowRoot.querySelector("img").src = newValue;
+        } else if (name === "name") {
+            this.shadowRoot.querySelector("img").alt = newValue;
+            this.shadowRoot.querySelector("h2").textContent = newValue;
+        }
+    }
+}
+
+customElements.define("organizer-element", Organizer, { extends: "article" });
